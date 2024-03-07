c1ffec44642e1bf92e6d612feafbe82cc5918c69
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-03-06 21:09:33 -0400

    rest api endpoints  added

--- taxis/urls.py
+++ taxis/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from taxis import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('taxis/', views.taxi_list),
     path('taxis/<int:pk>/', views.taxi_detail),
+    path('taxis/<int:taxi_id>/<str:date>/', views.taxi_trayectories, name='taxi-trayectories'),
+    path('trayectories/', views.trayectory_list),
+    path('trayectories/<int:pk>/', views.trayectory_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
