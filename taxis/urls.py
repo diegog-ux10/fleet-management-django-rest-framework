1e9435b2f6ad2a61942df62e1a3a1c8923d5d1d7
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-03-08 21:30:55 -0400

    added name to urls

--- taxis/urls.py
+++ taxis/urls.py
@@ -3,10 +3,10 @@ from taxis import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('taxis/', views.TaxiList.as_view()),
-    path('taxis/<int:pk>/', views.TaxiDetails.as_view()),
+    path('taxis/', views.TaxiList.as_view(), name='taxi-list'),
+    path('taxis/<int:pk>/', views.TaxiDetails.as_view(), name='taxi-detail'),
     path('trayectories/', views.TrayectoriesList.as_view()),
-    path('trayectories/<int:pk>/', views.TrayectoryDetails.as_view()),
+    path('trayectories/<int:pk>/', views.TrayectoryDetails.as_view(), name='trayectories'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
